09a5d7ef13a1b332cc66c7351e2e1cd90e67fe71
AnnaKachmasheva <annakachmasheva@gmail.com>
2023-09-23 23:09:45 +0200

    logo onclick action

--- ui/src/components/general/AuthHeader.js
+++ ui/src/components/general/AuthHeader.js
@@ -1,13 +1,22 @@
 import React from "react";
 import logo from '../../assets/logo.png';
 import styles from './AuthHeader.module.scss';
+import {useNavigate} from "react-router-dom";
 
 
-const AuthHeader = () => (
-    <div className={'logo'}>
-        <img className={styles.logo} src={logo} alt={'logo'}/>
-    </div>
+const AuthHeader = () => {
 
-);
+    const navigate = useNavigate()
+
+    return (
+        <div className={'logo'}>
+            <img className={styles.logo}
+                 src={logo} alt={'logo'}
+                 onClick={() => {
+                     navigate(`/`)
+                 }}/>
+        </div>
+    )
+};
 
 export default AuthHeader;
